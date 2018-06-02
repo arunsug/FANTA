9e80c9246658468ce5f97eb6f1e57f37c2a64852
lokiw <yiw315@ucsd.edu>
2018-06-02 12:26:47 -0700

    before switch to Manage

--- app/src/main/java/com/example/android/projectfanta/Settings.java
+++ app/src/main/java/com/example/android/projectfanta/Settings.java
@@ -30,6 +30,7 @@ public class Settings extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+        finish();
     }
 
     private void googleSignOut() {
